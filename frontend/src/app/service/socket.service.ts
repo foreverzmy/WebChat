f62cbf145e5ce411a92387ff38e4cc399558c3fc
trc-forever <921255465@qq.com>
2017-05-10 20:06:55 +0800

    加入动画效果

--- frontend/src/app/service/socket.service.ts
+++ frontend/src/app/service/socket.service.ts
@@ -3,10 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import * as io from 'socket.io-client';
 
 import { SOCKET } from './api';
-<<<<<<< HEAD
-=======
 // import { SOCKET_PROD } from './api';
->>>>>>> 913942fa06b9693d4b040b4ef3ffd0d2fcea42c9
 
 @Injectable()
 export class SocketService {
